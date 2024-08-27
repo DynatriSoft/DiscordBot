0f18049015b30ea59a3b079ddfa4b5eea2b017c8
André Nunes <andre@ottocar.co.uk>
2024-08-27 23:56:11 +0100

    feat: remove modal ping

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,35 +1,11 @@
-const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     category: 'utility',
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Opens a modal with some questions.'),
+        .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        // Create the modal
-        const modal = new ModalBuilder()
-            .setCustomId('myModal')
-            .setTitle('My Modal');
-
-        // Create the text input components
-        const favoriteColorInput = new TextInputBuilder()
-            .setCustomId('favoriteColorInput')
-            .setLabel("What's your favorite color?")
-            .setStyle(TextInputStyle.Short);
-
-        const hobbiesInput = new TextInputBuilder()
-            .setCustomId('hobbiesInput')
-            .setLabel("What's some of your favorite hobbies?")
-            .setStyle(TextInputStyle.Paragraph);
-
-        // An action row only holds one text input, so you need one action row per text input.
-        const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-        const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
-
-        // Add inputs to the modal
-        modal.addComponents(firstActionRow, secondActionRow);
-
-        // Show the modal to the user
-        await interaction.showModal(modal);
+        await interaction.reply('Pong!');
     },
 };
