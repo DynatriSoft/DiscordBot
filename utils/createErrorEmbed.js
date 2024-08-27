7957deadbe9620abaad4f5fe95e252e7deafa97a
André Nunes <andre@ottocar.co.uk>
2024-08-28 00:46:07 +0100

    improve error image

--- utils/createErrorEmbed.js
+++ utils/createErrorEmbed.js
@@ -1,24 +1,27 @@
 const { EmbedBuilder } = require('discord.js');
 
-function createErrorEmbed(title, description, color = '#FF0000') {
+function createErrorEmbed(title, description, color = '#FF4C4C') {
     if (!title) {
         title = 'An error occurred';
     }
     if (!description) {
-        description = 'An error occurred while executing this command. Please try again later.';
+        description = 'An unexpected error occurred while executing this command. Please try again later.';
     }
+
     if (title.length > 256) {
         title = `${title.slice(0, 253)}...`;
     }
     if (description.length > 2048) {
         description = `${description.slice(0, 2045)}...`;
     }
+
     return new EmbedBuilder()
         .setTitle(title)
         .setDescription(description)
         .setColor(color)
         .setTimestamp()
-        .setFooter({ text: 'An error occurred', iconURL: 'https://example.com/error-icon.png' });
+        .setThumbnail('https://s3.dynatrisoft.com/assets/logo/red.png')
+        .setFooter({ text: 'DynatriSoft Games', iconURL: 'https://s3.dynatrisoft.com/assets/logo/red.png' });
 }
 
 module.exports = { createErrorEmbed };
