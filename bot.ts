8d86544049c6a6c616262a14d541197eb81568b4
André Nunes <andre@ottocar.co.uk>
2024-08-27 22:41:03 +0100

    feat: improve commands slash refresh

--- bot.ts
+++ bot.ts
@@ -1,20 +1,55 @@
 import 'dotenv/config';
-import { Client, GatewayIntentBits } from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const TOKEN = process.env.TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
-});
+client.commands = new Collection();
 
-client.on('interactionCreate', async interaction => {
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction: { isChatInputCommand: () => any; client: { commands: { get: (arg0: any) => any; }; }; commandName: any; replied: any; deferred: any; followUp: (arg0: { content: string; ephemeral: boolean; }) => any; reply: (arg0: { content: string; ephemeral: boolean; }) => any; }) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'piiing') {
-        await interaction.reply('Pong!');
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
     }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
 });
 
 await client.login(TOKEN);
--- commands.ts
+++ commands.ts
@@ -1,24 +1,41 @@
-import 'dotenv/config';
-import { REST, Routes } from 'discord.js';
+import {REST, Routes} from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
 
 const TOKEN = process.env.TOKEN as string;
 const CLIENT_ID = process.env.CLIENT_ID as string;
 
-const commands = [
-    {
-        name: 'piiing',
-        description: 'Replies with Pong!',
-    },
-];
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-    console.error(error);
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
 }
+
+const rest = new REST().setToken(TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        await rest.put(
+            Routes.applicationCommands(CLIENT_ID),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
