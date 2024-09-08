5aded531ccda071169bf6ee58794cbe0c6c17b9c
André Nunes <hello@anuness.dev>
2024-09-08 21:50:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import 'dotenv/config';
-import http from "http";
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 const TOKEN = process.env.TOKEN;
 
+const htmlFilePath = path.join(__dirname, 'index.html');
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Bot is running\n');
+    fs.readFile(htmlFilePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Error loading the page');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
 });
 
 server.listen(process.env.PORT || 3000, async () => {
@@ -13,6 +24,7 @@ server.listen(process.env.PORT || 3000, async () => {
 
     const { ShardingManager } = require('discord.js');
     const manager = new ShardingManager('./bot.js', { token: TOKEN });
+    
     manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
     await manager.spawn();
 });
